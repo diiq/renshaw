9ba8d13191819409021eda70992c6724bc2817cf
Sam Bleckley <diiq.diiq@gmail.com>
2012-10-08 12:57:55 +1300

    Added color, gardening.

--- iso.js
+++ iso.js
@@ -1,44 +1,7 @@
+var preloaded = {};
+
 $(document).ready(
 function () {
-    var preloaded = {};
-
-    var preload = function () {
-        var j, k;
-        for(var i =0; i<arguments.length; i++){
-            j = new Image();
-            j.src = arguments[i];
-            k = $("<img>");
-            k.attr("src", arguments[i]);
-            preloaded[arguments[i]] = k.clone();
-            $("body").append(k.hide());
-        }
-    };
-    
-    preload("img/baobab.png",
-            "img/clockwork.png",
-            "img/clock.png",
-            "img/cclock.png",
-            "img/gleft.png",
-            "img/gren.png",
-            "img/gupup.png",
-            "img/gwswap.png",
-            "img/water.png",
-            "img/wgchange.png",
-            "img/wleft.png",
-            "img/wrigh.png",
-            "img/baobad.png",
-            "img/gdown.png",
-            "img/green.png",
-            "img/grigh.png",
-            "img/gwchange.png",
-            "img/save.png",
-            "img/saved.png",
-            "img/waterb.png",
-            "img/wdown.png",
-            "img/white.png",
-            "img/wren.png",
-            "img/wupup.png");
-
 
     /* This file provides a function to render a game board in iso:
 
@@ -70,39 +33,34 @@ function () {
                        {ding:ding});
 
 
+    /** Rendering **/
+
     // Dups! Fix yo'sel!    
-    var render_tile = function (x, y, tile, rx, ry){
-        var $tile = preloaded["img/"+tile.src].clone();        
-        $tile.attr("class", "tile");
-        $tile.css({left: max_left+(y*y_magic[0]+x*x_magic[0])+"px",
-                   top:  max_top+(y*y_magic[1]+x*x_magic[1]) +"px"});
-        // REMOVE FOLLOWING LINE FROM PRODUCTION CODE TODO WARNING
-        $tile.click(function(){grid.ren.x = rx; grid.ren.y = ry;render(grid);});
-        $("#grid").append($tile);
+    var render_obj = function (x, y, ol, ot, cls, src) {
+        var $obj = preloaded["img/"+src].clone();        
+        $obj.attr("class", cls);
+        $obj.css({left: max_left+(y*y_magic[0]+x*x_magic[0])+ol+"px",
+                   top:  max_top+(y*y_magic[1]+x*x_magic[1])+ot+"px"});
+        $("#grid").append($obj);
+        return $obj;
+    };
+
+    var render_tile = function (x, y, tile){
+        render_obj(x, y, 0, 0, "tile", tile.src);
     };
 
     var render_ren = function (ren){
-        // Some duplication here; gardening to do.
-        var $tile = preloaded["img/"+ren.src[ren.color]].clone();
-        $tile.attr("class", "ren");
-        var x = 9, y=ren.y;
-        $tile.css({left: max_left+(y*y_magic[0]+x*x_magic[0])+4+"px",
-                   top:  max_top+(y*y_magic[1]+x*x_magic[1])-90 +"px"});
-
+        render_obj(9, ren.y, 4, -90, "ren", ren.src[ren.color]);
+        // Shift bg to match
         $("#mask").css("background-position",  -ren.x*x_magic[0]+"px " + 
                                                -ren.x*x_magic[1]+"px");
-
-        $("#grid").append($tile);
-        $("#mask").css("background-color", {green:"#563", white:"#555"}[ren.color]);
+        $("#mask").css("background-color", {orang:"#653", 
+                                            green:"#563", 
+                                            white:"#555"}[ren.color]);
     };
 
     var render_special = function (x, y, tile){
-        var $tile = $("<img />");
-        $tile.attr("src", "img/"+tile.src);
-        $tile.attr("class", "tile");
-        $tile.css({left: max_left+(y*y_magic[0]+x*x_magic[0])+tile.offset[0]+"px",
-                   top:  max_top+(y*y_magic[1]+x*x_magic[1]) +tile.offset[1]+"px"});
-        $("#grid").append($tile);
+        render_obj(x, y, tile.offset[0], tile.offset[1], "tile", tile.src);
     };
 
     var render = function (grid) {
@@ -123,7 +81,7 @@ function () {
                   39:move("y", -1),   // right
                   40:move("x", -1),   // down
                   82:grid.load,       // r
-                  32:grid.minor_load  // space
+//                  32:story_next       // space
                  };
     $("body").keydown(function (e) {
                           if (keymap[e.which]) {
@@ -132,15 +90,26 @@ function () {
                           }
                       });
 
-    $("#load").click(function(){
-                         grid.mport($("#output").val());
-                         render(grid);
-                     });
-
     render(grid);
 
 });
 
 
-/* This is a dumb place for a preloader. */
-
+var preload = function () {
+    var j, k;
+    for(var i =0; i<arguments.length; i++){
+        j = new Image();
+        j.src = arguments[i];
+        k = $("<img>");
+        k.attr("src", arguments[i]);
+        preloaded[arguments[i]] = k.clone();
+        $("body").append(k.hide());
+    }
+}("img/baobab.png", "img/clockwork.png", "img/clock.png", "img/cclock.png", 
+  "img/gleft.png", "img/gren.png", "img/gupup.png", "img/gwswap.png", 
+  "img/water.png", "img/wgchange.png", "img/wleft.png", "img/wrigh.png", 
+  "img/baobad.png", "img/gdown.png", "img/green.png", "img/grigh.png", 
+  "img/gwchange.png", "img/save.png", "img/saved.png", "img/waterb.png", 
+  "img/wdown.png", "img/white.png", "img/wren.png", "img/wupup.png", 
+  "img/orang.png", "img/oren.png", "img/owchange.png", "img/wochange.png", 
+  "img/gochange.png", "img/ogchange.png", "img/goswap.png", "img/owswap.png");
